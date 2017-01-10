a139f3a3cfe6b9648a42dda5b114fdbe8cd65ae5
Viktor Karabut <karabut.viktor@gmail.com>
2017-01-11 00:36:30 +0200

    Reformat code. Let's use 2 spaces intent.

--- app/src/main/java/com/blogspot/karabut/rescal/MainActivity.java
+++ app/src/main/java/com/blogspot/karabut/rescal/MainActivity.java
@@ -6,42 +6,45 @@ import android.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-	static final int[][] INITIAL_BANDS = {
-		{6, 5, 8, 1,},
-		{6, 5, 6, 4, 7,},
-		{6, 5 ,5, 4, 7, 2},
-	};
+  static final int[][] INITIAL_BANDS = {
+      {6, 5, 8, 1,},
+      {6, 5, 6, 4, 7,},
+      {6, 5, 5, 4, 7, 2},
+  };
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    //setContentView(R.layout.activity_main);
 
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+    final ActionBar actionBar = getActionBar();
+    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        for (int i = 0; i < 3; i++) {
-            Fragment fragment = new ResistorFragment();
-            Bundle args = new Bundle();
-            args.putInt(ResistorFragment.KEY_ID, i);
-            args.putIntArray(ResistorFragment.KEY_BANDS, INITIAL_BANDS[i]);
-            fragment.setArguments(args);
+    for (int i = 0; i < 3; i++) {
+      Fragment fragment = new ResistorFragment();
+      Bundle args = new Bundle();
+      args.putInt(ResistorFragment.KEY_ID, i);
+      args.putIntArray(ResistorFragment.KEY_BANDS, INITIAL_BANDS[i]);
+      fragment.setArguments(args);
 
-            ActionBar.Tab bandTab = actionBar
-                    .newTab()
-                    .setText(getTabTitle(i))
-                    .setTabListener(new FragmentTabListener(this, "4 band", fragment));
+      ActionBar.Tab bandTab = actionBar
+          .newTab()
+          .setText(getTabTitle(i))
+          .setTabListener(new FragmentTabListener(this, "4 band", fragment));
 
-            actionBar.addTab(bandTab);
-        }
+      actionBar.addTab(bandTab);
     }
+  }
 
-    private CharSequence getTabTitle(int position) {
-        switch (position) {
-            case 0: return getString(R.string.tab_band_4).toUpperCase();
-            case 1: return getString(R.string.tab_band_5).toUpperCase();
-            case 2: return getString(R.string.tab_band_6).toUpperCase();
-        }
-        return null;
+  private CharSequence getTabTitle(int position) {
+    switch (position) {
+      case 0:
+        return getString(R.string.tab_band_4).toUpperCase();
+      case 1:
+        return getString(R.string.tab_band_5).toUpperCase();
+      case 2:
+        return getString(R.string.tab_band_6).toUpperCase();
     }
+    return null;
+  }
 }
--- app/src/main/java/com/blogspot/karabut/utils/BoxBlur.java
+++ app/src/main/java/com/blogspot/karabut/utils/BoxBlur.java
@@ -6,88 +6,90 @@ import android.graphics.Bitmap;
 import android.util.Log;
 
 public class BoxBlur {
-	private static final int RED_OFFSET   = 0;
-	private static final int GREEN_OFFSET = 1;
-	private static final int BLUE_OFFSET  = 2;
-	private static final int ALPHA_OFFSET = 3;
-	private static final int BYTES_PER_PIXEL = 4;
-	
-	public static void append(Bitmap bitmap, int radius) {
-		if (Bitmap.Config.ARGB_8888 != bitmap.getConfig()) {
-			Log.e("BoxBlur", "Expected ARGB_8888 bitmap, but got " + bitmap.getConfig() + ". Aborting.");
-			return;
-		}
-		
-		int w = bitmap.getWidth();
-		int h = bitmap.getHeight();
-		
-		byte[] buf1 = new byte[w*h*BYTES_PER_PIXEL];
-		byte[] buf2 = new byte[w*h*BYTES_PER_PIXEL];
-		
-		bitmap.copyPixelsToBuffer(ByteBuffer.wrap(buf1));
-		
-		blurHorizontal(buf1, buf2, w, h, radius);
-		blurVertical(buf2, buf1, w, h, radius);
-		
-		bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(buf1));
-	}
-	
-	private static void blurHorizontal(byte[] src, byte[] dest, int w, int h, int radius) {
-		int size = radius*2 + 1;
-		int maxj = w - size;
-	
-		for (int i = 0; i < h; i++) {
-			for(int j = 0; j <= maxj; j++) {
-				int index = i*w*BYTES_PER_PIXEL + j*BYTES_PER_PIXEL;
-				int sumR = 0, sumG = 0, sumB = 0, sumA = 0;
-				for (int offset = 0; offset < size; offset++) {
-					int indexOffset = index + offset*BYTES_PER_PIXEL;
-					sumR += src[indexOffset + RED_OFFSET  ] & (0xff);
-					sumG += src[indexOffset + GREEN_OFFSET] & (0xff);
-					sumB += src[indexOffset + BLUE_OFFSET ] & (0xff);
-					sumA += src[indexOffset + ALPHA_OFFSET] & (0xff);
-				}
-				int indexCenter = index + radius*BYTES_PER_PIXEL;
-				dest[indexCenter + RED_OFFSET  ] = normalize(sumR, size, 0); 
-				dest[indexCenter + GREEN_OFFSET] = normalize(sumG, size, 0); 
-				dest[indexCenter + BLUE_OFFSET ] = normalize(sumB, size, 0); 
-				dest[indexCenter + ALPHA_OFFSET] = normalize(sumA, size, 0); 
-			}
-		}
-	}
-	
-	private static void blurVertical(byte[] src, byte[] dest, int w, int h, int radius) {
-		int size = radius*2 + 1;
-		int maxi = h - size;
-		
-		for (int i = 0; i <= maxi; i++) {
-			for(int j = 0; j < w; j++) {
-				int index = i*w*BYTES_PER_PIXEL + j*BYTES_PER_PIXEL;
-				int sumR = 0, sumG = 0, sumB = 0, sumA = 0;
-				for (int offset = 0; offset < size; offset++) {
-					int indexOffset = index + offset*w*BYTES_PER_PIXEL;
-					sumR += src[indexOffset + RED_OFFSET  ] & (0xff);
-					sumG += src[indexOffset + GREEN_OFFSET] & (0xff);
-					sumB += src[indexOffset + BLUE_OFFSET ] & (0xff);
-					sumA += src[indexOffset + ALPHA_OFFSET] & (0xff);
-				}
-				int indexCenter = index + radius*w*BYTES_PER_PIXEL;
-				dest[indexCenter + RED_OFFSET  ] = normalize(sumR, size, 0); 
-				dest[indexCenter + GREEN_OFFSET] = normalize(sumG, size, 0); 
-				dest[indexCenter + BLUE_OFFSET ] = normalize(sumB, size, 0); 
-				dest[indexCenter + ALPHA_OFFSET] = normalize(sumA, size, 0); 
-			}
-		}
-	}
-	
-	private static byte normalize(int v, int f, int o) {
-		v = v/f + o;
-		if (v < 0) {
-			return (byte)0x00;
-		} else if (v > 255) {
-			return (byte)0xff;
-		} else {
-			return (byte)v;
-		}
-	}
+  private static final int RED_OFFSET = 0;
+  private static final int GREEN_OFFSET = 1;
+  private static final int BLUE_OFFSET = 2;
+  private static final int ALPHA_OFFSET = 3;
+  private static final int BYTES_PER_PIXEL = 4;
+
+  public static void append(Bitmap bitmap, int radius) {
+    if (Bitmap.Config.ARGB_8888 != bitmap.getConfig()) {
+      Log.e("BoxBlur", "Expected ARGB_8888 bitmap, but got " + bitmap.getConfig() + ". Aborting.");
+      return;
+    }
+
+    int w = bitmap.getWidth();
+    int h = bitmap.getHeight();
+
+    byte[] buf1 = new byte[w * h * BYTES_PER_PIXEL];
+    byte[] buf2 = new byte[w * h * BYTES_PER_PIXEL];
+
+    bitmap.copyPixelsToBuffer(ByteBuffer.wrap(buf1));
+
+    blurHorizontal(buf1, buf2, w, h, radius);
+    blurVertical(buf2, buf1, w, h, radius);
+
+    bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(buf1));
+  }
+
+  private static void blurHorizontal(byte[] src, byte[] dest, int w, int h, int radius) {
+    int size = radius * 2 + 1;
+    int maxj = w - size;
+
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j <= maxj; j++) {
+        int index = i * w * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL;
+        int sumR = 0, sumG = 0, sumB = 0, sumA = 0;
+        for (int offset = 0; offset < size; offset++) {
+          int indexOffset = index + offset * BYTES_PER_PIXEL;
+          sumR += src[indexOffset + RED_OFFSET] & (0xff);
+          sumG += src[indexOffset + GREEN_OFFSET] & (0xff);
+          sumB += src[indexOffset + BLUE_OFFSET] & (0xff);
+          sumA += src[indexOffset + ALPHA_OFFSET] & (0xff);
+        }
+        int indexCenter = index + radius * BYTES_PER_PIXEL;
+        dest[indexCenter + RED_OFFSET] = normalize(sumR, size, 0);
+        dest[indexCenter + GREEN_OFFSET] = normalize(sumG, size, 0);
+        dest[indexCenter + BLUE_OFFSET] = normalize(sumB, size, 0);
+        dest[indexCenter + ALPHA_OFFSET] = normalize(sumA, size, 0);
+      }
+    }
+  }
+
+  private static void blurVertical(byte[] src, byte[] dest, int w, int h, int radius) {
+    int size = radius * 2 + 1;
+    int maxi = h - size;
+
+    for (int i = 0; i <= maxi; i++) {
+      for (int j = 0; j < w; j++) {
+        int index = i * w * BYTES_PER_PIXEL + j * BYTES_PER_PIXEL;
+        int sumR = 0, sumG = 0, sumB = 0, sumA = 0;
+        for (int offset = 0; offset < size; offset++) {
+          int indexOffset = index + offset * w * BYTES_PER_PIXEL;
+          sumR += src[indexOffset + RED_OFFSET] & (0xff);
+          sumG += src[indexOffset + GREEN_OFFSET] & (0xff);
+          sumB += src[indexOffset + BLUE_OFFSET] & (0xff);
+          sumA += src[indexOffset + ALPHA_OFFSET] & (0xff);
+        }
+        int indexCenter = index + radius * w * BYTES_PER_PIXEL;
+        dest[indexCenter + RED_OFFSET] = normalize(sumR, size, 0);
+        dest[indexCenter + GREEN_OFFSET] = normalize(sumG, size, 0);
+        dest[indexCenter + BLUE_OFFSET] = normalize(sumB, size, 0);
+        dest[indexCenter + ALPHA_OFFSET] = normalize(sumA, size, 0);
+      }
+    }
+  }
+
+  private static byte normalize(int v, int f, int o) {
+    v = v / f + o;
+    if (v < 0) {
+      return (byte) 0x00;
+    }
+    else if (v > 255) {
+      return (byte) 0xff;
+    }
+    else {
+      return (byte) v;
+    }
+  }
 }
