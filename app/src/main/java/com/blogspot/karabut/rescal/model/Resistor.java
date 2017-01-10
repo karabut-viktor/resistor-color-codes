d23565d6161da687e185e95344cd429821a33c59
Viktor Karabut <karabut.viktor@gmail.com>
2017-01-11 00:53:05 +0200

    Implement Resistor.getTolerance and Resistor.getResistance methods

--- app/src/main/java/com/blogspot/karabut/rescal/model/Resistor.java
+++ app/src/main/java/com/blogspot/karabut/rescal/model/Resistor.java
@@ -1,12 +1,40 @@
 package com.blogspot.karabut.rescal.model;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 public class Resistor {
+  private final static String DEFAULT_TOLERANCE = "20";
+
   private final List<Color> colors;
 
   public Resistor(Color... colors) {
     this.colors = Arrays.asList(colors);
+  }
+
+  public CharSequence getTolerance() {
+    if (colors.size() == 4)
+      return DEFAULT_TOLERANCE;
+    else
+      return colors.get(4).getTolerance();
+  }
+
+  public BigDecimal getResistance() {
+    BigDecimal value;
+    int mult;
+
+    if (colors.size() == 4) {
+      value = new BigDecimal(colors.get(0).getDigit() * 10 + colors.get(1).getDigit());
+      mult = colors.get(2).getMultiplier();
+    }  else  {
+      value = new BigDecimal(
+          + colors.get(1).getDigit() * 100
+          + colors.get(1).getDigit() * 10
+          + colors.get(1).getDigit() * 1);
+      mult = colors.get(3).getMultiplier();
+    }
+
+    return value.scaleByPowerOfTen(mult);
   }
 }
